5f29c8f0b3efae6cc8980d54166b22e457b395d7
Kxxxahyun <153481000+Kxxxahyun@users.noreply.github.com>
2024-10-21 00:06:02 +0900

    1021
    judges

--- judges.c
+++ judges.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// 밀리웨이즈
-// 이름:김영수,성별:남,소속:JYK 스튜디오,직함:음악 감독 및 프로듀서,전문분야:음악,메일:youngsu@outlook.com,전화:010-1234-5678
-// 이름:이지영,성별:여,소속:DK 엔터테인먼트,직함:댄스 감독,전문분야:댄스,메일:jiyounghere@outlook.com,전화:010-2345-6789
-// 이름:박민준,성별:남,소속:JM 엔터테인먼트,직함:보컬 감독,전문분야:보컬,메일:minjun@outlook.com,전화:010-3456-7890
-// 이름:송현우,성별:남,소속:Starlight Entertainment,직함:비주얼 디렉터,전문분야:비주얼,메일:hyunwoo@outlook.com,전화:010-4567-8901
-// 이름:박지영,성별:여,소속:음악평론가 협회,직함:음악 평론가,전문분야:음악 평론,메일:jiyoung@outlook.com,전화:010-5678-9012
-
 int main()
 {
     int num_judges;
-    char project_name[100];
-    char judges_array[6 * 1000];
+    char project_name[10];
+    char judges_array[100 * 1000];
     char buffer[1000];
 
+    // 심사자 풀 입력
     printf("####################################\n");
-    printf("#       심사자 풀 데이터 입력     #\n");
+    printf("#       심사자 풀 데이터 입력      #\n");
     printf("####################################\n");
-
     printf("> 참여 프로젝트: ");
     scanf("%99[^\n]", project_name);
     getchar();
@@ -28,31 +21,35 @@ int main()
     getchar();
 
     printf("> 선발 멤버 수: 4\n");
+
+    int judge_idx = 0;
     printf("++++++++++++++++++++++++++++++++++++\n");
     printf("%d명의 심사자 정보 입력을 시작합니다.\n", num_judges);
     printf("++++++++++++++++++++++++++++++++++++\n");
 
-    int i = 0;
-    while (i < num_judges)
+    while (judge_idx < num_judges)
     {
-        printf("*심사자 %d: ", i + 1);
+        printf("*심사자 %d: ", judge_idx + 1);
         fgets(buffer, 1000, stdin);
 
-        int len = 0;
-        while (buffer[len] != '\0' && buffer[len] != '\n')
+        int length = 0;
+        while (buffer[length] != '\0' && buffer[length] != '\n')
         {
-            len++;
+            length++;
         }
-        buffer[len] = '\0';
 
-        int j = 0; // strncpy 복사
-        while (j < 1000 - 1 && buffer[j] != '\0')
+        if (length == 0)
         {
-            judges_array[i * 1000 + j] = buffer[j];
-            j++;
+            printf("입력 누락!\n");
+            continue;
         }
-        judges_array[i * 1000 + j] = '\0';
-        i++;
+
+        for (int k = 0; k < length; k++)
+        {
+            judges_array[judge_idx * 1000 + k] = buffer[k];
+        }
+        judges_array[judge_idx * 1000 + length] = '\0';
+        judge_idx++;
     }
 
     printf("++++++++++++++++++++++++++++++++++++\n");
@@ -67,30 +64,28 @@ int main()
     if (pool_check == 'Y')
     {
         printf("####################################\n");
-        printf("#       심사자 풀 데이터 출력     #\n");
+        printf("#       심사자 풀 데이터 출력      #\n");
         printf("####################################\n");
         for (int i = 0; i < num_judges; i++)
         {
             printf("[심사자 %d]\n", i + 1);
-            char temp[1000];
-            int j = 0; // strncpy 복사
-            while (j < 1000 - 1 && judges_array[i * 1000 + j] != '\0')
+
+            // JSON 데이터 파싱
+            int start = 0;
+            for (int j = 0; j < 1000; j++)
             {
-                temp[j] = judges_array[i * 1000 + j];
-                j++;
-            }
-            temp[j] = '\0';
-            char *start = temp;
-            for (int k = 0; k < j; k++)
-            {
-                if (temp[k] == ',')
+                if (judges_array[i * 1000 + j] == ',' || judges_array[i * 1000 + j] == '\0')
                 {
-                    temp[k] = '\0';
-                    printf("%s\n", start);
-                    start = &temp[k + 1];
+                    for (int k = start; k < j; k++)
+                    {
+                        putchar(judges_array[i * 1000 + k]);
+                    }
+                    putchar('\n');
+                    start = j + 1;
+                    if (judges_array[i * 1000 + j] == '\0')
+                        break;
                 }
             }
-            printf("%s\n", start);
             printf("-----------------------------------\n");
         }
     }
@@ -98,8 +93,6 @@ int main()
     {
         printf("프로그램을 종료합니다.\n");
     }
-
-    getchar();
 
     return 0;
 }
